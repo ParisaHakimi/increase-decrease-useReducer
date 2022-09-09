3963b2702a8f508dd8760ce12d9fa9cc6045ee9c
Parisa Sohrabi <parisahs@gmail.com>
2022-09-09 14:06:23 -0400

    gave functionality by the use of usereducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useReducer } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const initialState = { count: 0 };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "increament":
+        return { count: state.count + 1 };
+      case "decreament":
+        return { count: state.count - 1 };
+    }
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="App">
+      <button onClick={() => dispatch({ type: "decreament" })}>-</button>
+      <span>{state.count}</span>
+      <button onClick={() => dispatch({ type: "increament" })}>+</button>
+    </div>
+  );
 }
 
 export default App;
